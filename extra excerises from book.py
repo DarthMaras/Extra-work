87723fd22ee0a210498e82b856b08999aab9cd51
DarthMaras <114573286+DarthMaras@users.noreply.github.com>
2022-10-05 18:01:14 +0100

    Add files via upload

---
+++ extra excerises from book.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# just doing some excerises from the book at nero
+a = 1
+a == 0
+print b = (a == 1)
+
+a = 1
+print (b = a**2 - a)
+
+a = 2
+print ( b = a**2 - a)
+
+# i used %magic in iphython tab and everything dissapeared
+# if you press q everything goes back to normal
+
+# %reset ask teacher about it
+
+print a = 1
+print a
+print b = a**2 - a
+print b
+print a = 2
+print (a)
+print (b)
+print (b = a**2 - a)
+print a, b
+print (a, b)
+
+# doesnt work for some reason?
+
+
+
+
+
+
+
+
+
+
+
+
+
